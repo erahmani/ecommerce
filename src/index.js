5639206b56f88335d84e029a06a3c4097943e9bc
erahmani <erahmani1369@gmail.com>
2024-10-30 08:30:01 +0330

    Step6: Connecting MongoDB with mongoose.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const mongoose = require('mongoose');
+
 dotenv.config({path:`${__dirname}\\.env`});
 
 const authRoutes = require("./routes/auth");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+mongoose.connect(process.env.MONGO_URI,{
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(()=> console.log('Connected to MongoDB'))
+.catch(err=>console.error('Could not connect to MongoDB', err));
 
 app.use(express.json());
 app.use("/auth", authRoutes);
